ac32265fcb991497785d421e151289ef9401fbd3
Adukani26 <adan.konse@strathmore.edu>
2025-07-11 22:51:48 +0300

    Update MainApplication.java

--- challenge6/MainApplication.java
+++ challenge6/MainApplication.java
@@ -2,9 +2,9 @@ public class MainApplication {
 
     public static void main(String[] args) {
     
-    Admin curAdmin = new Admin(1234, "Stephan", "xa1@LW42%3");
+    Admin curAdmin = new Admin(1234, "Oliver", "2x1@674gdbe");
     
-    User curUser = new User(4455, "John");
+    User curUser = new User(4455, "Johnny");
     
     curAdmin.performOperation(new Update()); // Admin Updates the data.
     
