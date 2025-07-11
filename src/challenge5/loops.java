e777e3da476420f59e5488e9e0b694d93ce3680a
Adukani26 <adan.konse@strathmore.edu>
2025-07-11 22:38:35 +0300

    Update loops.java

--- src/challenge5/loops.java
+++ src/challenge5/loops.java
@@ -2,7 +2,7 @@ package challenge5;
 
 import java.util.Scanner;
 
-public class Loops {
+public class loops {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
